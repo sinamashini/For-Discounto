418d93be95c3a41df59546c80447a4047c9e49c3
sinamashini <sina.mashini1993@gmail.com>
2022-04-11 16:19:26 +0430

    feat: adding new api for getting user

--- app/api/getUser.ts
+++ app/api/getUser.ts
@@ -1,8 +1,16 @@
 import { BlitzApiRequest, BlitzApiResponse } from "blitz"
+import db from "db";
 
-const handler = (req: BlitzApiRequest, res: BlitzApiResponse) => {
-  res.statusCode = 200
-  res.setHeader("Content-Type", "application/json")
-  res.end(JSON.stringify({ name: "John Doe" }))
+const handler = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      const users = await db.user.findMany({});
+      return res.status(200).send(users);
+    } catch (err) {
+      return res.status(500).send("Internal Server Error!");
+    }
+  } else {
+    return res.status(405).send("Method not allowed!");
+  }
 }
 export default handler
